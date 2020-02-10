f2cb345d57ab4d6545b215c7145d1d0c8de9e2c9
macleod2486 <macleod2486@gmail.com>
2020-02-10 00:58:15 -0600

    Code cleanup and library upgrades

--- app/src/main/java/com/macleod2486/packagetracker/fragments/Main.java
+++ app/src/main/java/com/macleod2486/packagetracker/fragments/Main.java
@@ -25,7 +25,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
