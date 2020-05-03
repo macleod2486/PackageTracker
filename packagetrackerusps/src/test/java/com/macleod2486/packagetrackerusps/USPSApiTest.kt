6d6155e189f5cd50434ba1e964a620bd4c237a3c
macleod2486 <macleod2486@gmail.com>
2020-05-02 21:48:13 -0500

    Added license

--- packagetrackerusps/src/test/java/com/macleod2486/packagetrackerusps/USPSApiTest.kt
+++ packagetrackerusps/src/test/java/com/macleod2486/packagetrackerusps/USPSApiTest.kt
@@ -1,14 +1,30 @@
+/*
+ *
+ *   PackageTrackerUSPS
+ *    a library that contains classes necessary to handle USPS data
+ *
+ *    Copyright (C) 2020  Manuel Gonzales Jr.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see [http://www.gnu.org/licenses/].
+ *
+ */
 package com.macleod2486.packagetrackerusps
 
 import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class USPSApiTest {
     @Test
     fun addition_isCorrect() {
